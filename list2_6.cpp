7a0782a9b009cb75b94403caed293ba53c6e88a9
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-20 12:05:37 +0900

    standard weight function.

--- list2_6.cpp
+++ list2_6.cpp
@@ -3,19 +3,34 @@
 using namespace std;
 
 int main() {
-    double height;
-    double weight;
-    double bmi;
+    const int STD_BMI = 22; // 標準BMI
+    double height; // 身長
+    double weight; // 体重
+    double bmi; // BMI
+    double stdWeight; // 標準体重
 
+    // キー入力をheightに格納する
     cout << "身長(cm)を入力してください: ";
     cin >> height;
 
+    // キー入力をweightに格納する
     cout << "体重を入力してください";
     cin >> weight;
 
+    // 身長をcm単位からm単位に変換する
     height /= 100;
 
+    // 身長と体重からBMIを計算する
     bmi = weight / height / height;
 
+    // 標準体重を計算する
+    stdWeight = STD_BMI * height * height;
+
+    // BMIを画面に表示する
     cout << "あなたの体重は、" << fixed << setprecision(1) << bmi << "です。" << endl;
+
+    // 標準体重を画面に表示する
+    cout << "あなたの標準体重は、" << stdWeight << "Kgです。" << endl;
+
+    return 0;
 }
