ddf038b306594dd8a1b037a47f9db5cf895c2c58
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-21 09:50:01 +0900

    Add getBmiHC.cpp

--- chapter4.h
+++ chapter4.h
@@ -9,6 +9,7 @@ struct HealthCheck {
 
 // BMIを求める関数のプロトタイプ宣言
 double getBmi(double height, double weight);
+double getBmi(const HealthCheck *phc);
 
 //二次方程式を解く関数のプロトタイプ宣言
 bool quadEq(double a, double b, double c, double *px1, double *px2);
---
+++ getBmiHC.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include "chapter4.h"
+
+double getBmi(const HealthCheck *phc) {
+    return getBmi(phc->height, phc->weight);
+}
