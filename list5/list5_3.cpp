f929fa3c75e73ec7b7c03cb545d1605a841d0981
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-22 16:23:19 +0900

    Done list5_３.cpp

--- list5/list5_3.cpp
+++ list5/list5_3.cpp
@@ -15,13 +15,38 @@ int main() {
         cout << "大きい。" << endl;
     }
     else if (s1 < s2) {
-        cout << "小さい。" endl;
+        cout << "小さい。" << endl;
     }
     else {
-        cout << "等しい。" endl;
+        cout << "等しい。" << endl;
     }
 
     // 文字列を連結する
     s3 = s1 + s2;
     cout << s3 << endl;
+
+    // 長さを求める
+    cout << s2.length() << endl;
+
+    // 5番目から3文字の部文字列を取り出す
+    cout << s3.substr(5, 3) << endl;
+
+    // 5文字目だけを取り出す
+    cout << s3[5] << endl;
+
+    // "na"という文字列を見つける
+    cout << s3.find("na") << endl;
+
+    // 文字列を空にする
+    s3.clear();
+
+    // 文字列がからであることを確認する
+    if (s3.empty()) {
+        cout << "空です。" << endl;
+    }
+    else {
+        cout << "空ではありません。" << endl;
+    }
+
+    return 0;
 }
