cae9ae824f441bde33506e76a2fadc4908e41abf
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-23 09:16:10 +0900

    Add HealthChecker.cpp

---
+++ list5/HealthChecker.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include "HealthChecker.h"
+
+// BMIを返すメンバ関数の実装
+double HealthChecker::getBmi() {
+
+}
+
+// コンストラクタの実装
+HealthChecker::HealthChecker(string name, double height, double weight) {
+    
+}
