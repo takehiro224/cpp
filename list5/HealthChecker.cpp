4266fecf566d5a5aee0d9904324c80065186a4cc
Takehiro Watanabe <mogu.224@gmail.com>
2023-03-23 09:35:50 +0900

    Update HealthChecker.cpp

--- list5/HealthChecker.cpp
+++ list5/HealthChecker.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 // BMIを返すメンバ関数の実装
 double HealthChecker::getBmi() {
-
+    // まだBMIが計算されていなかったら計算する
+    if (this->bmi == 0) {
+        double mHeight = this->height / 100;
+        this->bmi = this->weight / mHeight / mHeight;
+    }
 }
 
 // コンストラクタの実装
 HealthChecker::HealthChecker(string name, double height, double weight) {
-    
+
 }
