5fb3eaf528cb90c8de93ee9f398515ca5e3ff9c0
Takehiro Watanabe <mogu.224@gmail.com>
2023-04-01 05:56:59 +0900

    Update list5_10.cpp

--- list5/list5_10.cpp
+++ list5/list5_10.cpp
@@ -4,5 +4,9 @@ using namespace std;
 #include "HealthChecker.h"
 
 int main() {
+    // HealthCheckerクラスのインスタンスを生成する
+    HealthChecker yamada("山田一郎", 170, 67.5);
+    // BMIの値を表示する
+    cout << yamada.getName() << "さんのBMIは、" << yamada.getBmi() << "です。" << endl;
     return 0;
 }
