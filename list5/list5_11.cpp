0d2396d968e5266f97190cd2da8b8f1bdd6eff55
Takehiro Watanabe <mogu.224@gmail.com>
2023-04-04 06:50:08 +0900

    Update list5_11.cpp

--- list5/list5_11.cpp
+++ list5/list5_11.cpp
@@ -4,5 +4,17 @@ using namespace std;
 #include "HealthChecker.h"
 
 int main() {
+    const int DATA_NUM = 3;
+    // HealthCheckerクラスをデータ型とした配列
+    HealthChecker people[DATA_NUM] = {
+        HealthChecker("山田", 170, 50.0);
+        HealthChecker("佐藤", 170, 50.0);
+        HealthChecker("鈴木", 170, 50.0);
+    }
+
+    for (int i = 0; i < DATA_NUM; i++) {
+        // BMIを表示する
+        cout << people[i].getName() << "さんのBMIは、" << people[i].getBmi() << "です" << endl;
+    }
     return 0;
 }
